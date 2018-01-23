3700f61e4b37b0b22352477b203e030a81cb8708
Josh Pizel <joshpizel@AD.DOE.CARLETON.CA>
2018-01-23 15:26:03 -0500

    first iteration of graphics. only first image creation

--- Mass-model/Mass model/plotMass.m
+++ Mass-model/Mass model/plotMass.m
@@ -1,10 +1,37 @@
 function plotMass
 %function will plot current position of masses and create a picture of such
 global D_X D_Y mass
-
+nImages = length(D_X);
 massFig=figure('Name','Mass simulation');
 
-position = mass(
+positionX = mass(:,1);
+positionY = mass(:,2);
+size = mass(:,3);
+
+xlimit = [min(positionX)-1 max(positionX)+1];
+ylimit = [min(positionY)-1 max(positionY)+1];
+
+%initial picture
+scatter(positionX,positionY,size,'filled')
+xlim(xlimit)
+ylim(ylimit)
+
+Frame = getframe(massFig);
+
+
+image{1} = frame2im(Frame);
+
+filename = 'image.gif';
+
+[A,map] = rgb2ind(image{1},256);
+imwrite(A,map,filename,'gif','loopCount',Inf,'DelayTime',1);
+%else imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',1);
+
+%main plotting structure
+%for i = 1;1;length(D_X)
+    
+    
+%end
 
 
 end
